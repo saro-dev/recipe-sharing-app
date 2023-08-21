583665485a65e69c5063bbe53c69c6d9834cefa4
saro-dev <codersaro@gmail.com>
2023-08-21 21:43:02 +0530

    Add new folder and its contents

--- database/index.js
+++ database/index.js
@@ -9,6 +9,8 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
 
 // Middleware
 app.use(bodyParser.json());
@@ -16,7 +18,7 @@ app.use(cors());
 app.use('/uploads/', express.static('uploads'));
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://codersaro:Sarorosy12@cluster0.av48khu.mongodb.net/', {
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.av48khu.mongodb.net/recipe-sharing-app`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
