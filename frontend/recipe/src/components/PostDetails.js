9278736519f003c1b530e20e205364676db7e1a9
Saravanan S <codersaro@gmail.com>
2023-10-08 17:46:00 +0530

    Update PostDetails.js

--- frontend/recipe/src/components/PostDetails.js
+++ frontend/recipe/src/components/PostDetails.js
@@ -272,6 +272,12 @@ const handleToggleLike = async () => {
         </button>
       </div>
       <hr></hr>
+          {!loggedInUser && (
+        <div className="fixed top-0 left-0 right-0 z-999 bg-red-100 p-2 rounded text-red-600 mb-4">
+          Signup to view more dishes! Please <Link to="/login">Log In</Link> or <Link to="/signup">Sign Up</Link> to access additional features.
+        </div>
+      )}
+
       <h2 className="text-xl font-semibold mb-4 mt-2 ">
         Post By{' '}
         <button
