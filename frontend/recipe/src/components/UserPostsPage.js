6842048c340942f33c47bd83504082243367f6d1
saro-dev <codersaro@gmail.com>
2024-01-19 22:10:18 +0530

    first commit

--- frontend/recipe/src/components/UserPostsPage.js
+++ frontend/recipe/src/components/UserPostsPage.js
@@ -51,20 +51,7 @@ const UserPostsPage = () => {
             onClick={() => navigate(-1)}
             className="back-nav"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 text-gray-600"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M10 19l-7-7m0 0l7-7m-7 7h18"
-              />
-            </svg>Back
+            &#x2190; Back
           </button>
         </div>
         <h2 className="text-xl font-semibold mb-4 flex">
